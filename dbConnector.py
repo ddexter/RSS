5f37a486d75f5212a2623d1e0117ad3243169ed1
Dan Dexter <dcdexter@gmail.com>
2013-01-26 17:44:32 -0800

    Sometimes the connector does not connect to the DB on its first attempt.  Added retry handling.

--- dbConnector.py
+++ dbConnector.py
@@ -7,11 +7,20 @@ class DBConnector:
     def connect(self, host, dbname, user, password):
         # Connect to database
         self._conn = None
-        try:
-            connStr = "host='%s' dbname='%s' user='%s' password='%s'" % (host, dbname, user, password)
-            self._conn = psycopg2.connect(connStr)
-        except:
-            print("Unable to connect")
-            self._conn = None
+
+        # Sometimes there are random problems connecting, try 100 times.
+        # Realistically should connect on the first 1 or 2 attempts
+        for i in range(100):
+            try:
+                connStr = "host='%s' dbname='%s' user='%s' password='%s'" % (host, dbname, user, password)
+                self._conn = psycopg2.connect(connStr)
+            except:
+                if i == 99:
+                    print("Unable to connect")
+                    self._conn = None
+                continue
+
+            # Stop trying to connect once link established
+            break
 
         return self._conn
